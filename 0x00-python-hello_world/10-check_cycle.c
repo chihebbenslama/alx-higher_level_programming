9965ad1b827aab151874c699e85faadf2eceb9ed
chihebbenslama <chihebbenslama@gmail.com>
2023-09-05 01:22:27 +0000

    Linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/*
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
+ *
+ * Return: 0
+ */
+
+int check_cycle(listint_t *list)
+{
+	return (0);
+}
