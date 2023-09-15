3ab831b077b37d53e7ab9c33fa0055e06605043c
chihebbenslama <chihebbenslama@gmail.com>
2023-09-15 23:22:27 +0000

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,6 @@ def square_matrix_simple(matrix=[]):
     nm = matrix.copy()
 
     for i in range(len(matrix)):
-        nm[i] = list(map(lambda x: x**2,matrix[i]))
+        nm[i] = list(map(lambda x: x**2, matrix[i]))
 
     return (nm)
